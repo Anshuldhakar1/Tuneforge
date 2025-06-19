9d3791e3da557b623601e3c3bb5cabc66d03cb12
Anshul <anshuldhakar03@gmail.com>
2025-06-19 09:18:07 +0530

    Add navigation links and create Page2 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.tsx';
+import Page2 from './pages/Page2.tsx';
 
 function App() {
 
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/page2">Page 2</Link>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </div>
     </div>
   );
 }
